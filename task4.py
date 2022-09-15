b23886f24385ca181f67e411960f2ca063a38bbe
Leo_Amv <leonidamvrosov@yandex.ru>
2022-09-15 21:42:58 +0500

    task4 completed!

--- task4.py
+++ task4.py
@@ -1,1 +1,12 @@
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+
+try:
+    string = ''
+    number = int(input('\nEnter number:\t'))
+    while number != 0:
+        string += str(number % 2)
+        number = int(number/2)
+    print(f'\nBinary number:\t{string[::-1]}\n')
+except ValueError:
+    print('Incorrect data! You must enter number, try again!')
